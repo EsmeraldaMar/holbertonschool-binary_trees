dd6884882a624b641b725a181e8cc03abdd56a2f
Uwe Flumm <ucflumm@gmail.com>
2023-09-21 22:32:59 +1000

    added notation

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -11,20 +11,32 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 {
 	binary_tree_t *tmp;
 
+	/* Sanity Checks */
 	if (first == NULL || second == NULL)
 		return (NULL);
+
+	/* If the first and second tree are the same */
 	if (first == second)
 		return ((binary_tree_t *)first);
+
+	/**
+	 *  Assign temporary tree to second
+	 *  Typecast to remove const
+	 */
 	tmp = (binary_tree_t *)second;
+
 	while (first != NULL)
 	{
 		while (second != NULL)
 		{
+			/* Cast the return without const */
 			if (first == second)
 				return ((binary_tree_t *)first);
+			/* Move second up the tree */
 			second = second->parent;
 		}
 		second = tmp;
+		/* Move first up the tree */
 		first = first->parent;
 	}
 	return (NULL);
